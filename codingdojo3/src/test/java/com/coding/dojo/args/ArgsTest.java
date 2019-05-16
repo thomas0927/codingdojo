df883a7fcd2c3f3c0ad9023b885035e2ccf170b9
thomas0927 <thomas0927@163.com>
2019-05-16 12:56:53 +0800

    完成Args的Schema编写

--- codingdojo3/src/test/java/com/coding/dojo/args/ArgsTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/args/ArgsTest.java
@@ -7,12 +7,13 @@ public class ArgsTest {
   @Test
   public void should_return_correct_value_by_args() {
     String argsAsText = "-l -p 8080 -x 1234 -d /usr/log";
-    String schemasAsText = "l:boolean p:integer d:string x:integer f:boolean";
+    String schemasAsText = "l:boolean p:integer d:string x:integer f:boolean w:integer";
     Schemas schemas = new Schemas(schemasAsText);
     Args args = new Args(schemas, argsAsText);
     Assert.assertEquals(args.getValue('l'), Boolean.TRUE);
     Assert.assertEquals(args.getValue('p'), 8080);
     Assert.assertEquals(args.getValue('x'), 1234);
+    Assert.assertEquals(args.getValue('w'), 0);
     Assert.assertEquals(args.getValue('f'), Boolean.FALSE);
   }
 }
