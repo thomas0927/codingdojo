0e0edbc6cf7cbbd83926699562d460d321df18c1
thomas0927 <thomas0927@163.com>
2019-05-16 16:55:59 +0800

    Default Changelist

---
+++ codingdojo3/src/test/java/com/coding/dojo/args/ValueParserTest.java
@@ -0,0 +1,18 @@
+package com.coding.dojo.args;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class ValueParserTest {
+
+  @Test(dataProvider = "parser_value")
+  public void should_parser_value_by_type(String value, Class type, Object expected) {
+    Assert.assertEquals(ValueParser.parser(value, type), expected);
+  }
+
+  @DataProvider
+  public Object[][] parser_value() {
+    return new Object[][] {{"", Boolean.class, Boolean.TRUE}};
+  }
+}
