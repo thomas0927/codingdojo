72bb66508abff952b43241a277f4bee09a228a86
thomas0927 <thomas0927@163.com>
2019-05-16 12:56:24 +0800

    完成Args的Schema编写

--- codingdojo3/src/main/java/com/coding/dojo/args/Args.java
+++ codingdojo3/src/main/java/com/coding/dojo/args/Args.java
@@ -4,9 +4,11 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Args {
+  private final Argument defaultArgument;
   private Schemas schemas;
   private Map<Character, Argument> argsMap;
 
@@ -18,9 +20,11 @@ public class Args {
                 Collectors.toMap(
                     args -> args.charAt(0),
                     args -> new Argument(args.charAt(0), StringUtils.trim(args.substring(1)))));
+    this.defaultArgument = new Argument('-', null);
   }
 
   public Object getValue(Character flag) {
-    return schemas.getArgsValue(flag, argsMap.get(flag).getValue());
+    return schemas.getArgsValue(
+        flag, Optional.ofNullable(argsMap.get(flag)).orElse(defaultArgument).getValue());
   }
 }
--- codingdojo3/src/test/java/com/coding/dojo/args/ArgsTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/args/ArgsTest.java
@@ -7,11 +7,12 @@ public class ArgsTest {
   @Test
   public void should_return_correct_value_by_args() {
     String argsAsText = "-l -p 8080 -x 1234 -d /usr/log";
-    String schemasAsText = "l:boolean p:integer d:string x:integer";
+    String schemasAsText = "l:boolean p:integer d:string x:integer f:boolean";
     Schemas schemas = new Schemas(schemasAsText);
     Args args = new Args(schemas, argsAsText);
     Assert.assertEquals(args.getValue('l'), Boolean.TRUE);
     Assert.assertEquals(args.getValue('p'), 8080);
     Assert.assertEquals(args.getValue('x'), 1234);
+    Assert.assertEquals(args.getValue('f'), Boolean.FALSE);
   }
 }
