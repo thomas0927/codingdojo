ca62aa4a8aea6b00ccd07c082bcd83de9711946b
thomas0927 <thomas0927@163.com>
2019-05-16 16:58:21 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/args/ValueParser.java
+++ codingdojo3/src/main/java/com/coding/dojo/args/ValueParser.java
@@ -1,7 +1,18 @@
 package com.coding.dojo.args;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 public class ValueParser {
+
+  private static Map<Class, Function<String, Object>> converters = new HashMap<>();
+
+  static {
+    converters.put(Boolean.class, s -> "".equalsIgnoreCase(s));
+  }
+
   public static Object parser(String value, Class type) {
-    return Boolean.TRUE;
+    return converters.get(type).apply(value);
   }
 }
