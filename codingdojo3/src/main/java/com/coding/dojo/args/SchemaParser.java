e4a68f3824f608f6ee01ad26a29a75e787f0252d
thomas0927 <thomas0927@163.com>
2019-05-16 16:04:59 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/args/SchemaParser.java
+++ codingdojo3/src/main/java/com/coding/dojo/args/SchemaParser.java
@@ -1,8 +1,20 @@
 package com.coding.dojo.args;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SchemaParser {
 
-  public static Schema parser(String schemaAsText) {
-    return new Schema(Boolean.class, Boolean.FALSE);
+  public static final String REGEX = ":";
+  private static Map<String, Schema> converters = new HashMap<>();
+
+  static {
+    converters.put("boolean", new Schema<>(Boolean.class, Boolean.FALSE));
+  }
+
+  public static Schema<Boolean> parser(String schemaAsText) {
+    String[] schemas = schemaAsText.split(REGEX);
+    String type = schemas[1];
+    return converters.get(type);
   }
 }
