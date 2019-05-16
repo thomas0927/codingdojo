922902339437f68f78f6aaecb33d6949c462e14e
thomas0927 <thomas0927@163.com>
2019-05-16 17:16:50 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/args/Schemas.java
+++ codingdojo3/src/main/java/com/coding/dojo/args/Schemas.java
@@ -1,9 +1,20 @@
 package com.coding.dojo.args;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Schemas {
-  public Schemas(String schemasAsText) {}
+
+  private Map<Character, Schema> schemaMap;
+
+  public Schemas(String schemasAsText) {
+    this.schemaMap =
+        Arrays.stream(schemasAsText.split(" "))
+            .collect(Collectors.toMap(s -> s.charAt(0), s -> SchemaParser.parser(s)));
+  }
 
   public Object getArgsValue(Character flag, String value) {
-    return Boolean.TRUE;
+    return schemaMap.get(flag).getValue(value);
   }
 }
