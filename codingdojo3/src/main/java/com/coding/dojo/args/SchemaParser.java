14f3b2c63813e2bf0b63c54a82ddc25d975455a0
thomas0927 <thomas0927@163.com>
2019-05-16 11:46:40 +0800

    完成Args的Schema编写

--- codingdojo3/src/main/java/com/coding/dojo/args/SchemaParser.java
+++ codingdojo3/src/main/java/com/coding/dojo/args/SchemaParser.java
@@ -1,7 +1,24 @@
 package com.coding.dojo.args;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
 public class SchemaParser {
+  public static final String REGEX = ":";
+  public static Map<String, Function<String, Object>> converters = new HashMap<>();
+
+  static {
+    converters.put(
+        "boolean", s -> new Schema(Boolean.class, (null == s) ? Boolean.FALSE : Boolean.TRUE));
+  }
+
   public static Schema parser(String schemaAsText) {
-    return new Schema(Boolean.class, Boolean.FALSE);
+    String[] schemas = schemaAsText.split(REGEX);
+    String type = schemas[1];
+    final String value = (schemas.length > 2) ? schemas[2] : null;
+    return (Schema)
+        Optional.ofNullable(converters.get(type)).map(parser -> parser.apply(value)).orElse(null);
   }
 }
