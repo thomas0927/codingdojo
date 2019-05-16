8cd5ab556038e2821b79958f462f970bdc80775a
thomas0927 <thomas0927@163.com>
2019-05-16 12:01:37 +0800

    完成Args的Schema编写

--- codingdojo3/src/main/java/com/coding/dojo/args/Args.java
+++ codingdojo3/src/main/java/com/coding/dojo/args/Args.java
@@ -1,9 +1,26 @@
 package com.coding.dojo.args;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Args {
-  public Args(Schemas schemas, String argsAsText) {}
+  private Schemas schemas;
+  private Map<Character, Argument> argsMap;
+
+  public Args(Schemas schemas, String argsAsText) {
+    this.schemas = schemas;
+    this.argsMap =
+        Arrays.stream(argsAsText.substring(1).split("-"))
+            .collect(
+                Collectors.toMap(
+                    args -> args.charAt(0),
+                    args -> new Argument(args.charAt(0), StringUtils.trim(args.substring(1)))));
+  }
 
   public Object getValue(Character flag) {
-    return Boolean.TRUE;
+    return schemas.getArgsValue(flag, argsMap.get(flag).getValue());
   }
 }
