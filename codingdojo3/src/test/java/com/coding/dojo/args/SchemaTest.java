cf8f4f70452215c7b98d917442e01fda0196d47c
thomas0927 <thomas0927@163.com>
2019-05-16 15:40:50 +0800

    Default Changelist

--- codingdojo3/src/test/java/com/coding/dojo/args/SchemaTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/args/SchemaTest.java
@@ -11,6 +11,16 @@ public class SchemaTest {
     Assert.assertEquals(schema.getValue(), expected);
   }
 
+  @Test(dataProvider = "given_schema")
+  public void can_return_given_value(Schema schema, String value, Object expected) {
+    Assert.assertEquals(schema.getValue(value), expected);
+  }
+
+  @DataProvider
+  public Object[][] given_schema() {
+    return new Object[][] {{new Schema(Boolean.class, Boolean.FALSE), "", Boolean.TRUE}};
+  }
+
   @DataProvider
   public Object[][] default_schema() {
     return new Object[][] {
