7a5debb54a9826c0b95b8e4dae077affcb771f6b
thomas0927 <thomas0927@163.com>
2019-05-16 15:45:09 +0800

    Default Changelist

---
+++ codingdojo3/src/main/java/com/coding/dojo/args/ValueParser.java
@@ -0,0 +1,7 @@
+package com.coding.dojo.args;
+
+public class ValueParser {
+  public static Object parser(String value, Class type) {
+    return Boolean.TRUE;
+  }
+}
---
+++ codingdojo3/src/test/java/com/coding/dojo/args/ValueParserTest.java
@@ -0,0 +1,17 @@
+package com.coding.dojo.args;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class ValueParserTest {
+  @Test(dataProvider = "parser_value")
+  public void can_parser_value_by_class(String value, Class type, Object expected) {
+    Assert.assertEquals(ValueParser.parser(value, type), expected);
+  }
+
+  @DataProvider
+  public Object[][] parser_value() {
+    return new Object[][] {{"", Boolean.class, Boolean.TRUE}};
+  }
+}
