0f0728ac1157d93eff525860d999a94fff938f18
thomas0927 <thomas0927@163.com>
2019-05-16 16:18:27 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/args/Args.java
+++ codingdojo3/src/main/java/com/coding/dojo/args/Args.java
@@ -1,9 +1,27 @@
 package com.coding.dojo.args;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Args {
-  public Args(Schemas schemas, String argsAsText) {}
+  public static final String REGEX = "-";
+  private Schemas schemas;
+  private Map<Character, Argument> argsMap;
+
+  public Args(Schemas schemas, String argsAsText) {
+    this.schemas = schemas;
+    this.argsMap =
+        Arrays.stream(argsAsText.substring(1).split(REGEX))
+            .collect(
+                Collectors.toMap(
+                    s -> s.charAt(0),
+                    s -> new Argument(s.charAt(0), StringUtils.trim(s.substring(1)))));
+  }
 
   public Object getValue(Character flag) {
-    return Boolean.TRUE;
+    return schemas.getArgsValue(flag, argsMap.get(flag).getValue());
   }
 }
