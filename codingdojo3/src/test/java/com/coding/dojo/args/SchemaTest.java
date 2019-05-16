3d0fb124b8cdb9e56e6cee228365af024ff4dbaf
thomas0927 <thomas0927@163.com>
2019-05-16 11:22:03 +0800

    完成Args的Schema编写

--- codingdojo3/src/test/java/com/coding/dojo/args/SchemaTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/args/SchemaTest.java
@@ -5,18 +5,17 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class SchemaTest {
-  @Test(dataProvider = "schema_check")
-  public void should_return_default_value_by_schema(
-      Schema schema, String value, Object defaultValue, Object expected) {
+  @Test(dataProvider = "schema_check_default")
+  public void should_return_default_value_by_schema(Schema schema, Object defaultValue) {
     Assert.assertEquals(schema.getValue(), defaultValue);
-    Assert.assertEquals(schema.getValue(value), expected);
   }
 
   @DataProvider
-  public Object[][] schema_check() {
+  public Object[][] schema_check_default() {
     return new Object[][] {
-      {new Schema(Boolean.class, Boolean.FALSE), "", Boolean.FALSE, Boolean.TRUE},
-      {new Schema(Boolean.class, Boolean.FALSE), null, Boolean.FALSE, Boolean.FALSE}
+      {new Schema(Boolean.class, Boolean.FALSE), Boolean.FALSE},
+      {new Schema(Boolean.class, Boolean.FALSE), Boolean.FALSE},
+      {new Schema(Integer.class, 0), 0}
     };
   }
 }
