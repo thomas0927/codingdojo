5a4b316afcece59f4e66ab95c8f603a27d514648
thomas0927 <thomas0927@163.com>
2019-05-27 17:21:16 +0800

    Default Changelist

--- codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBussTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBussTest.java
@@ -10,6 +10,10 @@ public class FizzBussTest {
   @ParameterizedTest(name = "should return {1} when given {0}")
   @CsvSource({"1,1", "3,Fizz"})
   public void should_return_correct_answer(Integer number, String answer) {
-    assertEquals(answer, new FizzBuzz().say(number));
+    assertEquals(answer, getSay(number));
+  }
+
+  private String getSay(Integer number) {
+    return new FizzBuzz().say(number);
   }
 }
