5141113c28db26b5d511785a9135cf25bfa705ca
thomas0927 <thomas0927@163.com>
2019-05-27 17:17:52 +0800

    Anagram

--- codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
+++ codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
@@ -1,41 +1,7 @@
 package com.coding.dojo.fizzbuzz;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 public class FizzBuzz {
   public String say(Integer number) {
-    String[] results = getRulesResult(number);
-    return getComponentRuleResult(number, results);
-  }
-
-  private String getComponentRuleResult(Integer number, String[] results) {
-    return Arrays.stream(results)
-        .filter(v -> !v.isEmpty())
-        .reduce(String::concat)
-        .orElse(String.valueOf(number));
-  }
-
-  private String[] getRulesResult(Integer number) {
-    List<Executable> rules = Rules.all();
-    return rules.stream().map(rule -> rule.exec(number)).toArray(String[]::new);
-  }
-
-  private String getFizzRuleResult(Integer number) {
-    return (number % 3 == 0 || isContains(number, "3")) ? "Fizz" : "";
-  }
-
-  private String getBuzzRuleResult(Integer number) {
-    return (number % 5 == 0 || isContains(number, "5")) ? "Buzz" : "";
-  }
-
-  private boolean isContains(Integer number, String s) {
-    return String.valueOf(number).contains(s);
-  }
-
-  public String print(int count) {
-    return IntStream.range(1, count + 1).mapToObj(this::say).collect(Collectors.joining("\n"));
+    return "1";
   }
 }
--- codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBussTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBussTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class FizzBuzzTest {
-  @ParameterizedTest(name = "should return {1} when give{0}")
-  @CsvSource({"1,1", "3,Fizz", "5,Buzz", "15,FizzBuzz", "13,Fizz"})
+public class FizzBussTest {
+
+  @ParameterizedTest(name = "should return {1} when given {0}")
+  @CsvSource({"1,1"})
   public void should_return_correct_answer(Integer number, String answer) {
-    FizzBuzz fizzBuzz = new FizzBuzz();
-    assertEquals(answer, fizzBuzz.say(number));
+    assertEquals(answer, new FizzBuzz().say(number));
   }
 }
