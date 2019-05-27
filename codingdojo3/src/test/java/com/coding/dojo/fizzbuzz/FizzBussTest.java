59ba2f1932cc5615a73fcfc761992f9c14f243f8
thomas0927 <thomas0927@163.com>
2019-05-27 17:26:44 +0800

    Default Changelist

--- codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBussTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBussTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBussTest {
 
   @ParameterizedTest(name = "should return {1} when given {0}")
-  @CsvSource({"1,1", "3,Fizz", "5,Buzz", "15,FizzBuzz", "13,Fizz"})
+  @CsvSource({"1,1", "3,Fizz", "5,Buzz", "15,FizzBuzz", "13,Fizz", "31,Fizz"})
   public void should_return_correct_answer(Integer number, String answer) {
     assertEquals(answer, getSay(number));
   }
