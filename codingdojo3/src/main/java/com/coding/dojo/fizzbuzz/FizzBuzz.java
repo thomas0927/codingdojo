310e9e084904edfa3ab1e2359639df4bcfe622b3
thomas0927 <thomas0927@163.com>
2019-05-27 15:41:57 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
+++ codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
@@ -1,33 +1,7 @@
 package com.coding.dojo.fizzbuzz;
 
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 public class FizzBuzz {
-  public String say(int number) {
-
-    if (isDivisible(number, 3) && isDivisible(number, 5)) {
-      return "FizzBuzz";
-    }
-    if (contains(number, 3) || isDivisible(number, 3)) {
-      return "Fizz";
-    }
-    if (contains(number, 5) || isDivisible(number, 5)) {
-      return "Buzz";
-    }
-
-    return String.valueOf(number);
-  }
-
-  private boolean contains(int number, int i) {
-    return number / 10 == i || number % 10 == i;
-  }
-
-  private boolean isDivisible(int number, int i) {
-    return number % i == 0;
-  }
-
-  public String print() {
-    return IntStream.range(1, 101).mapToObj(this::say).collect(Collectors.joining("\n"));
+  public String say(Integer number) {
+    return "1";
   }
 }
--- codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBuzzTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBuzzTest.java
@@ -1,34 +1,14 @@
 package com.coding.dojo.fizzbuzz;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
-
-  @ParameterizedTest(name = "should_return {1} given {0}")
-  @CsvSource({"1,1", "3,Fizz", "5,Buzz", "15,FizzBuzz", "13,Fizz", "31,Fizz", "52,Buzz"})
-  public void should_return_fizz_buzz_list_correct(int number, String answer) {
-    assertEquals(getSay(number), answer);
-  }
-
-  private String getSay(int number) {
-    return new FizzBuzz().say(number);
-  }
-
-  @Test
-  public void should_print_fizz_buzz_list_correctly() {
-    assertEquals(100, splitOutput().length);
-    assertEquals("1", getOutputLine(1));
-  }
-
-  private String getOutputLine(int number) {
-    return splitOutput()[number - 1];
-  }
-
-  private String[] splitOutput() {
-    return new FizzBuzz().print().split("\n");
+  @ParameterizedTest(name = "should return {1} given {0}")
+  @CsvSource({"1,1"})
+  public void should_answer_correct_fizz_buzz_number(Integer number, String answer) {
+    assertEquals(new FizzBuzz().say(number), answer);
   }
 }
