137023f5ea0843032abd907ec8d8662d2f39680e
thomas0927 <thomas0927@163.com>
2019-05-27 11:26:23 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
+++ codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
@@ -6,8 +6,12 @@ import java.util.stream.IntStream;
 public class FizzBuzz {
   public String say(int number) {
     if (number % 3 == 0) {
-        return "Fizz";
+      return "Fizz";
     }
+    if (number % 5 == 0) {
+      return "Buzz";
+    }
+
     return String.valueOf(number);
   }
 
--- codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBuzzTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBuzzTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest {
 
   @ParameterizedTest(name = "should_return {1} given {0}")
-  @CsvSource({"1,1", "3,Fizz"})
+  @CsvSource({"1,1", "3,Fizz", "5,Buzz"})
   public void should_return_fizz_buzz_list_correct(int number, String answer) {
     assertEquals(getSay(number), answer);
   }
