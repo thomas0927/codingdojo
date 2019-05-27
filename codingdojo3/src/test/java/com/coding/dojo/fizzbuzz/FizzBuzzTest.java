dc7244a0657acd09adf331a2a0b8aaf80bd6ffb5
thomas0927 <thomas0927@163.com>
2019-05-27 18:02:03 +0800

    Default Changelist

--- codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBuzzTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBuzzTest.java
@@ -8,7 +8,7 @@ import static org.testng.AssertJUnit.assertEquals;
 public class FizzBuzzTest {
 
   @ParameterizedTest(name = "should return {1} when given {0}")
-  @CsvSource({"1,1"})
+  @CsvSource({"1,1", "3,Fizz"})
   public void should_return_correct_answer(Integer number, String answer) {
     assertEquals(answer, new FizzBuzz().say(number));
   }
