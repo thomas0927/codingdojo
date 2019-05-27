182db10b99f8eb71519263864ed00525cd63e7a1
thomas0927 <thomas0927@163.com>
2019-05-27 17:26:34 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
+++ codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,10 @@ package com.coding.dojo.fizzbuzz;
 public class FizzBuzz {
   public String say(Integer number) {
     if (number % 3 == 0 && number % 5 == 0) {
-        return "FizzBuzz";
+      return "FizzBuzz";
+    }
+    if (number % 10 == 3 || number / 10 == 3) {
+        return "Fizz";
     }
     if (isDivisible(number, 3)) {
       return "Fizz";
