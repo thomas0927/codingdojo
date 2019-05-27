23dca416ffe68eaf8d3a860902501a3e9b3d8232
thomas0927 <thomas0927@163.com>
2019-05-27 17:40:29 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
+++ codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
@@ -5,13 +5,27 @@ public class FizzBuzz {
     if (isDivisibleOrCaptainsBy(number, 3) && isDivisibleOrCaptainsBy(number, 5)) {
       return "FizzBuzz";
     }
-    if (isDivisibleOrCaptainsBy(number, 3)) {
-      return "Fizz";
-    }
-    if (isDivisibleOrCaptainsBy(number, 5)) {
-      return "Buzz";
+    String result1 = getFizz(number, 3);
+    String result2 = getBuzz(number, 5);
+    String result = String.join("", result1, result2);
+    if (!result.isEmpty()) {
+      return result;
     }
     return String.valueOf(number);
+  }
+
+  private String getFizz(Integer number, int i) {
+    if (isDivisibleOrCaptainsBy(number, i)) {
+      return "Fizz";
+    }
+    return "";
+  }
+
+  private String getBuzz(Integer number, int i) {
+    if (isDivisibleOrCaptainsBy(number, i)) {
+      return "Buzz";
+    }
+    return "";
   }
 
   private boolean isDivisibleOrCaptainsBy(Integer number, int i) {
