12d84acc8b41d84d89855714e56360cfe7668739
thomas0927 <thomas0927@163.com>
2019-05-27 11:29:33 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
+++ codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
@@ -5,6 +5,9 @@ import java.util.stream.IntStream;
 
 public class FizzBuzz {
   public String say(int number) {
+    if (number % 3 == 0 && number % 5 == 0) {
+        return "FizzBuzz";
+    }
     if (number % 3 == 0) {
       return "Fizz";
     }
