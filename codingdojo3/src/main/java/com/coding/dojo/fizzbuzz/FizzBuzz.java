b3b371511be2a9e2fcf0f89c00afa6cbb0cd8ded
thomas0927 <thomas0927@163.com>
2019-05-27 15:50:29 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
+++ codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
@@ -3,17 +3,23 @@ package com.coding.dojo.fizzbuzz;
 public class FizzBuzz {
   public String say(Integer number) {
     String result1 = getFizzRuleResult(number);
-    if (!result1.isEmpty()) {
-      return result1;
+    String result2 = getBuzzRuleResult(number);
+    String result = result1 + result2;
+    if (!result.isEmpty()) {
+      return result;
     }
     return String.valueOf(number);
   }
 
   private String getFizzRuleResult(Integer number) {
-    return (number % 3 == 0 || isContains(number)) ? "Fizz" : "";
+    return (number % 3 == 0 || isContains(number, "3")) ? "Fizz" : "";
   }
 
-  private boolean isContains(Integer number) {
-    return String.valueOf(number).contains("3");
+  private String getBuzzRuleResult(Integer number) {
+    return (number % 5 == 0 || isContains(number, "5")) ? "Buzz" : "";
+  }
+
+  private boolean isContains(Integer number, String s) {
+    return String.valueOf(number).contains(s);
   }
 }
