837637404ac03af349f419b4563d620acdf7cf84
thomas0927 <thomas0927@163.com>
2019-05-27 18:06:54 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
+++ codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
@@ -2,12 +2,25 @@ package com.coding.dojo.fizzbuzz;
 
 public class FizzBuzz {
   public String say(Integer number) {
-    if (number % 3 == 0) {
-      return "Fizz";
+    String[] result = new String[] {getFizz(number), getBuzz(number)};
+    String result1 = String.join("", result);
+    if (!result1.isEmpty()) {
+      return result1;
     }
+    return String.valueOf(number);
+  }
+
+  private String getBuzz(Integer number) {
     if (number % 5 == 0) {
       return "Buzz";
     }
-    return String.valueOf(number);
+    return "";
+  }
+
+  private String getFizz(Integer number) {
+    if (number % 3 == 0) {
+      return "Fizz";
+    }
+    return "";
   }
 }
