e53a162a19f1be3db0c1e983a0bf3d73fd4e2963
thomas0927 <thomas0927@163.com>
2019-05-28 17:28:36 +0800

    Default Changelist

--- codingdojo3/src/test/java/com/coding/dojo/args/SchemaTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/args/SchemaTest.java
@@ -1,12 +1,18 @@
 package com.coding.dojo.args;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class SchemaTest {
-  @Test
-  public void should_get_schema_default_value() {
-    Schema schema = new Schema(Boolean.class, Boolean.FALSE);
-    Assert.assertEquals(schema.getValue(), Boolean.FALSE);
+  @Test(dataProvider = "schema_default_value")
+  public void should_get_schema_default_value(Class clz, Object resultValue, Object defaultValue) {
+    Schema schema = new Schema(clz, defaultValue);
+    Assert.assertEquals(schema.getValue(), resultValue);
+  }
+
+  @DataProvider
+  public Object[][] schema_default_value() {
+    return new Object[][] {{Boolean.class, Boolean.FALSE, Boolean.FALSE}};
   }
 }
