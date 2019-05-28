6d3c38d21036144feeaa2d360de3430f2c6484bc
thomas0927 <thomas0927@163.com>
2019-05-28 18:10:53 +0800

    Default Changelist

--- codingdojo3/src/test/java/com/coding/dojo/args/SchemasTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/args/SchemasTest.java
@@ -1,12 +1,21 @@
 package com.coding.dojo.args;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class SchemasTest {
-  @Test
-  public void should_get_correct_value_of_schema() {
-    Schemas schemas = new Schemas("l:boolean");
-    Assert.assertEquals(schemas.getArgsValue('l', ""), Boolean.TRUE);
+  @Test(dataProvider = "get_correct_value_of_schema")
+  public void should_get_correct_value_of_schema(
+      String schemasAsText, Character flag, String value, Object resultValue) {
+    Schemas schemas = new Schemas(schemasAsText);
+    Assert.assertEquals(schemas.getArgsValue(flag, value), resultValue);
+  }
+
+  @DataProvider
+  public Object[][] get_correct_value_of_schema() {
+    return new Object[][] {
+      {"l:boolean", 'l', "", Boolean.TRUE},
+    };
   }
 }
