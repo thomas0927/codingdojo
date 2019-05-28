dea9c3baf92a5eb749eb9f1359f921c926c26382
thomas0927 <thomas0927@163.com>
2019-05-28 17:25:41 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/args/Schema.java
+++ codingdojo3/src/main/java/com/coding/dojo/args/Schema.java
@@ -1,23 +1,16 @@
 package com.coding.dojo.args;
 
 public class Schema<T> {
-
   private Class<T> clz;
+
+  public T getValue() {
+    return defaultValue;
+  }
+
   private T defaultValue;
 
   public Schema(Class<T> clz, T defaultValue) {
     this.clz = clz;
     this.defaultValue = defaultValue;
-  }
-
-  public T getValue() {
-    return this.defaultValue;
-  }
-
-  public T getValue(String value) {
-    if (null == value) {
-      return getValue();
-    }
-    return (T) ValueParser.parser(value, this.clz);
   }
 }
--- codingdojo3/src/test/java/com/coding/dojo/args/SchemaTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/args/SchemaTest.java
@@ -1,34 +1,12 @@
 package com.coding.dojo.args;
 
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class SchemaTest {
-  @Test(dataProvider = "return_default_value")
-  public void should_return_default_value(Schema schema, Object defaultValue) {
-    Assert.assertEquals(schema.getValue(), defaultValue);
-  }
-
-  @Test(dataProvider = "return_given_value")
-  public void should_return_given_value(Schema schema, String value, Object expected) {
-    Assert.assertEquals(schema.getValue(value), expected);
-  }
-
-  @DataProvider
-  public Object[][] return_given_value() {
-    return new Object[][] {
-      {new Schema(Boolean.class, Boolean.FALSE), "", Boolean.TRUE},
-      {new Schema(Integer.class, 0), "1008", 1008},
-      {new Schema(String.class, ""), "1008", "1008"},
-    };
-  }
-
-  @DataProvider
-  public Object[][] return_default_value() {
-    return new Object[][] {
-      {new Schema(Boolean.class, Boolean.FALSE), Boolean.FALSE},
-      {new Schema(Integer.class, 0), 0},
-    };
+  @Test
+  public void should_get_schema_default_value() {
+    Schema schema = new Schema(Boolean.class, Boolean.FALSE);
+    Assert.assertEquals(schema.getValue(), Boolean.FALSE);
   }
 }
