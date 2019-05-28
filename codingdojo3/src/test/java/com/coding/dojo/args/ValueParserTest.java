280e1fe0b1d99476fc99162a5cc1c4b25bec54a1
thomas0927 <thomas0927@163.com>
2019-05-28 17:41:43 +0800

    Default Changelist

--- codingdojo3/src/test/java/com/coding/dojo/args/ValueParserTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/args/ValueParserTest.java
@@ -1,11 +1,20 @@
 package com.coding.dojo.args;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class ValueParserTest {
-  @Test
-  public void should_parser_value_from_input_text() {
-    Assert.assertEquals(ValueParser.parser("", Boolean.class), Boolean.TRUE);
+  @Test(dataProvider = "parser_value_from_input_text")
+  public void should_parser_value_from_input_text(String valueText, Class clz, Object resultValue) {
+    Assert.assertEquals(ValueParser.parser(valueText, clz), resultValue);
+  }
+
+  @DataProvider
+  public Object[][] parser_value_from_input_text() {
+    return new Object[][] {
+      {"", Boolean.class, Boolean.TRUE},
+      {"808", Integer.class, 808},
+    };
   }
 }
