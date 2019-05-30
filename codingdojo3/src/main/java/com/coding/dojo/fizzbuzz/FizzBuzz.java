35346decef5ef65f9d9d850778cfc288d378b5cd
thomas0927 <thomas0927@163.com>
2019-05-30 14:33:43 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
+++ codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,10 @@ package com.coding.dojo.fizzbuzz;
 public class FizzBuzz {
   public String say(int i) {
 
-    return "1";
+    return Rules.all().stream()
+        .map(rule -> rule.exec(i))
+        .reduce(String::concat)
+        .filter(s -> !s.isEmpty())
+        .orElse(String.valueOf(i));
   }
 }
--- codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBuzzTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBuzzTest.java
@@ -1,11 +1,20 @@
 package com.coding.dojo.fizzbuzz;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class FizzBuzzTest {
-  @Test
-  public void should_answer_fizz_buzz() {
-    Assert.assertEquals(new FizzBuzz().say(1), "1");
+  @Test(dataProvider = "fizz_buzz_nubmer")
+  public void should_answer_fizz_buzz(int number, String expected) {
+    Assert.assertEquals(new FizzBuzz().say(number), expected);
+  }
+
+  @DataProvider
+  public Object[][] fizz_buzz_nubmer() {
+    return new Object[][] {
+      {1, "1"},
+      {3, "Fizz"},
+    };
   }
 }
