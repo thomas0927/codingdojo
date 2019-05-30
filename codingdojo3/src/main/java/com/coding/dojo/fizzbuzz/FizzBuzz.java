7d3ee1994d339473a637fb6e116c8cce0de180e4
thomas0927 <thomas0927@163.com>
2019-05-30 14:28:26 +0800

    Default Changelist

---
+++ codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,8 @@
+package com.coding.dojo.fizzbuzz;
+
+public class FizzBuzz {
+  public String say(int i) {
+
+    return "1";
+  }
+}
---
+++ codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+package com.coding.dojo.fizzbuzz;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FizzBuzzTest {
+  @Test
+  public void should_answer_fizz_buzz() {
+    Assert.assertEquals(new FizzBuzz().say(1), "1");
+  }
+}
