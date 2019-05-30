5bfd6369f7ba1b29300667909b81d5da2ddb47c8
thomas0927 <thomas0927@163.com>
2019-05-30 16:35:32 +0800

    Default Changelist

--- codingdojo3/src/test/java/com/coding/dojo/args/ArgsTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/args/ArgsTest.java
@@ -8,5 +8,6 @@ public class ArgsTest {
   public void should_return_correct_value() {
     Assert.assertEquals(new Args("-l", "l:&").getValue('l'), Boolean.TRUE);
     Assert.assertEquals(new Args("-l -p 8080", "l:& p:@").getValue('p'), 8080);
+    Assert.assertEquals(new Args("-l -p 8080 -d /log", "l:& p:@ d:#").getValue('d'), "/log");
   }
 }
