8286672aa565e9b347b85890fefd61c8824ab64f
thomas0927 <thomas0927@163.com>
2019-05-30 11:00:23 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
+++ codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
@@ -2,11 +2,18 @@ package com.coding.dojo.fizzbuzz;
 
 public class FizzBuzz {
   public Object say(Integer number) {
+    String result = "";
     if (number % 3 == 0) {
-      return "Fizz";
+      result = "Fizz";
+    }
+    if (!result.isEmpty()) {
+        return result;
     }
     if (number % 5 == 0) {
-        return "Buzz";
+      result = "Buzz";
+    }
+    if (!result.isEmpty()) {
+        return result;
     }
     return String.valueOf(number);
   }
