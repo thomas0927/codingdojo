2eb01a700c9bb87d8257e04b7157a835072a548f
thomas0927 <thomas0927@163.com>
2019-05-30 13:31:42 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
+++ codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,14 @@
 package com.coding.dojo.fizzbuzz;
 
+import java.util.List;
+
 public class FizzBuzz {
-  public String say(int i) {
-    return "1";
+  public String say(int number) {
+    List<Executable> rules = Rules.all();
+    return rules.stream()
+        .map(rule -> rule.exec(number))
+        .reduce(String::concat)
+        .filter(s -> !s.isEmpty())
+        .orElse(String.valueOf(number));
   }
 }
