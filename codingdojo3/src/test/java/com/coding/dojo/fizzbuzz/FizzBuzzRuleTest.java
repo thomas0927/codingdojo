afec1ee19f5b4c45d2d46a3cc21f80188998b0dc
thomas0927 <thomas0927@163.com>
2019-05-30 14:03:21 +0800

    Default Changelist

--- codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBuzzRuleTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBuzzRuleTest.java
@@ -1,12 +1,21 @@
 package com.coding.dojo.fizzbuzz;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class FizzBuzzRuleTest {
-  @Test
-  public void should_get_fizz_buzz_rule() {
+  @DataProvider
+  public Object[][] fizz_buzz_rule() {
+    return new Object[][] {
+      {1, ""},
+      {3, "Fizz"},
+    };
+  }
+
+  @Test(dataProvider = "fizz_buzz_rule")
+  public void should_get_fizz_buzz_rule(int number, String expected) {
     Executable executable = FizzBuzzRule.build(3, "Fizz");
-    Assert.assertEquals(executable.exec(1), "");
+    Assert.assertEquals(executable.exec(number), expected);
   }
 }
