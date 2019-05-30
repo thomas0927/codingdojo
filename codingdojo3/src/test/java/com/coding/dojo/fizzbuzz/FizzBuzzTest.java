30935616abf12681a9927e0a177b12ab52a108a4
thomas0927 <thomas0927@163.com>
2019-05-30 13:33:36 +0800

    Default Changelist

--- codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBuzzTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/fizzbuzz/FizzBuzzTest.java
@@ -13,9 +13,7 @@ public class FizzBuzzTest {
   @DataProvider
   public Object[][] fizz_buzz_answer() {
     return new Object[][] {
-      {1, "1"},
-      {3, "Fizz"},
-      {5, "Buzz"},
+      {1, "1"}, {3, "Fizz"}, {5, "Buzz"}, {15, "FizzBuzz"}, {51, "FizzBuzz"},
     };
   }
 }
