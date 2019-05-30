dc027c53ba3881eeadfccfb8cf08bee6cbdad59e
thomas0927 <thomas0927@163.com>
2019-05-30 16:01:33 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/args/Schema.java
+++ codingdojo3/src/main/java/com/coding/dojo/args/Schema.java
@@ -1,23 +1,15 @@
 package com.coding.dojo.args;
 
 public class Schema<T> {
-  private Class<T> clz;
+  private final Class clz;
+  private final T defaultValue;
 
-  public T getValue() {
-    return defaultValue;
-  }
-
-  private T defaultValue;
-
-  public Schema(Class<T> clz, T defaultValue) {
+  public Schema(Class clz, T defaultValue) {
     this.clz = clz;
     this.defaultValue = defaultValue;
   }
 
-  public T getValue(String s) {
-    if (null == s) {
-      return getValue();
-    }
-    return ValueParser.parser(s, this.clz);
+  public T getValue() {
+    return this.defaultValue;
   }
 }
--- codingdojo3/src/test/java/com/coding/dojo/args/SchemaTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/args/SchemaTest.java
@@ -1,37 +1,11 @@
 package com.coding.dojo.args;
 
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class SchemaTest {
-  @Test(dataProvider = "schema_default_value")
-  public void should_get_schema_default_value(Class clz, Object resultValue, Object defaultValue) {
-    Schema schema = new Schema(clz, defaultValue);
-    Assert.assertEquals(schema.getValue(), resultValue);
-  }
-
-  @Test(dataProvider = "parser_schema_value")
-  public void should_parser_schema_value(
-      Class clz, Object defaultValue, String inputValue, Object resultValue) {
-    Schema schema = new Schema(clz, defaultValue);
-    Assert.assertEquals(schema.getValue(inputValue), resultValue);
-  }
-
-  @DataProvider
-  public Object[][] parser_schema_value() {
-    return new Object[][] {
-      {Boolean.class, Boolean.FALSE, "", Boolean.TRUE},
-      {Integer.class, 0, "8080", 8080}
-    };
-  }
-
-  @DataProvider
-  public Object[][] schema_default_value() {
-    return new Object[][] {
-      {Boolean.class, Boolean.FALSE, Boolean.FALSE},
-      {Integer.class, 0, 0},
-      {String.class, "/log", "/log"}
-    };
+  @Test
+  public void should_get_default_value_by_schema() {
+    Assert.assertEquals(new Schema(Boolean.class, Boolean.FALSE).getValue(), Boolean.FALSE);
   }
 }
