520de59bf421b3d8113b8d95de04b1874c7df962
thomas0927 <thomas0927@163.com>
2019-05-30 16:26:50 +0800

    Default Changelist

--- codingdojo3/src/test/java/com/coding/dojo/args/SchemasTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/args/SchemasTest.java
@@ -8,5 +8,6 @@ public class SchemasTest {
   public void should_get_schema_value_by_input() {
     Assert.assertEquals(new Schemas("l:&").getArgsValue('l', ""), Boolean.TRUE);
     Assert.assertEquals(new Schemas("l:& d:@").getArgsValue('d', "100"), 100);
+    Assert.assertEquals(new Schemas("l:& d:@ p:#").getArgsValue('p', "100"), "100");
   }
 }
