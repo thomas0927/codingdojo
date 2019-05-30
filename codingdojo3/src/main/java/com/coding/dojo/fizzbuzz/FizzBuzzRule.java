4ca96cc7d7bdf4907b65cbc5c0450039e2ca675f
thomas0927 <thomas0927@163.com>
2019-05-30 15:26:28 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzzRule.java
+++ codingdojo3/src/main/java/com/coding/dojo/fizzbuzz/FizzBuzzRule.java
@@ -16,13 +16,14 @@ public class FizzBuzzRule implements Executable {
 
   @Override
   public String exec(int number) {
-    if (number % 10 == this.in || number / 10 == this.in) {
-        return answer;
-    }
-    if (isDivisible(number)) {
-      return this.answer;
+    if (isContains(number) || isDivisible(number)) {
+      return answer;
     }
     return "";
+  }
+
+  private boolean isContains(int number) {
+    return number % 10 == this.in || number / 10 == this.in;
   }
 
   private boolean isDivisible(int number) {
