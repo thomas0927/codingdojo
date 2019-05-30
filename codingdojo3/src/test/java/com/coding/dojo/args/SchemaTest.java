775c99651e371d361d4c9dcf581283ea356e4b6b
thomas0927 <thomas0927@163.com>
2019-05-30 16:02:12 +0800

    Default Changelist

--- codingdojo3/src/test/java/com/coding/dojo/args/SchemaTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/args/SchemaTest.java
@@ -7,5 +7,6 @@ public class SchemaTest {
   @Test
   public void should_get_default_value_by_schema() {
     Assert.assertEquals(new Schema(Boolean.class, Boolean.FALSE).getValue(), Boolean.FALSE);
+    Assert.assertEquals(new Schema(Integer.class, 0).getValue(), 0);
   }
 }
